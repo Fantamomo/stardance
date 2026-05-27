3c8404b9f025d2636d682abeb2664ba7d093ca9d
Kartikey Chauhan <me@cskartikey.dev>
2026-05-27 20:56:45 +0530

    feat(voting): first pass on the voting UI.
    This does not match the figma. This is just an MVP and it'll be adapted to the design in a few hours

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,56 @@
 class VotesController < ApplicationController
+  def new
+    authorize Vote
+    load_assignment
+  end
+
+  def create
+    authorize Vote
+
+    @assignment = current_user.vote_assignments.assigned.find(params.require(:vote_assignment_id))
+    @vote = @assignment.submit_vote(vote_params)
+
+    if @vote.persisted?
+      redirect_to new_vote_path, notice: "Vote submitted."
+    else
+      @ship_event = @assignment.ship_event
+      @project = @ship_event.project
+      load_timeline_posts
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def load_assignment
+      @assignment = Vote::Assignment.assign_to(current_user)
+      if @assignment
+        @ship_event = @assignment.ship_event
+        @project = @ship_event.project
+        @vote = Vote.new(ship_event: @ship_event, project: @project)
+        load_timeline_posts
+      end
+    end
+
+    def load_timeline_posts
+      assigned_ship_post = @ship_event.post
+      @timeline_posts = []
+      return unless assigned_ship_post
+
+      @timeline_posts = @project.posts
+        .includes(postable: [ :attachments_attachments ])
+        .where("posts.created_at <= ?", assigned_ship_post.created_at)
+        .order(created_at: :desc)
+        .select { |post| post.postable.present? }
+        .reject { |post| post.postable_type == "Post::ShipEvent" && post.postable.certification_status == "rejected" }
+    end
+
+    def vote_params
+      params.require(:vote).permit(
+        :originality_score,
+        :technical_score,
+        :usability_score,
+        :storytelling_score,
+        :reason
+      )
+    end
 end
